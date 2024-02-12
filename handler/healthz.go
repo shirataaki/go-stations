d50b94fe9759a68dae94f8f768ae75af9e9f013e
久保佳子 <kubo@kuboyoshikonoMacBook-Air.local>
2024-02-12 15:43:14 +0900

    station05まで終了

--- handler/healthz.go
+++ handler/healthz.go
@@ -1,13 +1,17 @@
 package handler
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/TechBowl-japan/go-stations/model"
 )
 
 // A HealthzHandler implements health check endpoint.
-type HealthzHandler struct{}
+type HealthzHandler struct {
+	Message string
+}
 
 // NewHealthzHandler returns HealthzHandler based http.Handler.
 func NewHealthzHandler() *HealthzHandler {
@@ -16,5 +20,16 @@ func NewHealthzHandler() *HealthzHandler {
 
 // ServeHTTP implements http.Handler interface.
 func (h *HealthzHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	_ = &model.HealthzResponse{}
+	// 1. HealthzResponse を変数に代入
+	res := &model.HealthzResponse{Message: "OK"} // 1. Message の Field に OK という文字を入れる
+
+	// 2. JSONにシリアライズして書き込み
+	w.Header().Set("Content-Type", "application/json")
+	encoder := json.NewEncoder(w)
+
+	err := encoder.Encode(res) // 3. Encodeメソッドを呼び出し
+	if err != nil {
+		// 4. シリアライズ失敗時のエラーログ出力
+		log.Println("Failed to encode HealthzResponse:", err)
+	}
 }
--- handler/router/router.go
+++ handler/router/router.go
@@ -3,10 +3,17 @@ package router
 import (
 	"database/sql"
 	"net/http"
+
+	"github.com/TechBowl-japan/go-stations/handler"
 )
 
 func NewRouter(todoDB *sql.DB) *http.ServeMux {
 	// register routes
 	mux := http.NewServeMux()
+
+	// HealthzHandlerのエンドポイントを登録
+	healthzHandler := handler.NewHealthzHandler() // HealthzHandlerのインスタンスを作成
+	mux.Handle("/healthz", healthzHandler)        // /healthz のエンドポイントに healthzHandler を割り当て
+
 	return mux
 }
